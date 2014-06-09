fa776d6733337816d0bdee55dc816ccaf2db2cf4
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-06-09 09:49:33 -0700

    Modified proper_parenthetics to include more cases for when there are even number of parenthsis but not matching. There is still error when matching parenthesis dont come in order.

--- proper_parenthetics.py
+++ proper_parenthetics.py
@@ -1,6 +1,6 @@
 __author__ = 'eyuelabebe'
 
-def _proper_parent():
+def _proper_parent(_file):
 
     matching_parenthesis = {'{': '}', '(': ')', '[': ']'}
     all_parenthesis = ['{', '}', '(', ')', '[', ']']
@@ -11,7 +11,7 @@ def _proper_parent():
 
     _stop = False
 
-    with open('test.txt', 'r') as test_file:
+    with open(_file, 'r') as test_file:
         while not _stop:
             file = list(test_file.read())
 
@@ -22,22 +22,34 @@ def _proper_parent():
                 if letter in all_parenthesis:
                     collection.append(letter)
 
-            for i in collection:
-                if i in matching_parenthesis.keys():
-                    open_parenthesis.append(i)
-                else:
-                    closed_parenthesis.append(i)
+            if len(collection) % 2 == 0:
+                count = 0
+                _full = len(collection)
+                _iter = len(collection) / 2
 
-            if len(open_parenthesis) == len(closed_parenthesis):
-                result = 0
-                _stop = True
+                for i in range(_iter):
+                    if matching_parenthesis[collection[_iter - 1 - i]] == collection[_iter + i]:
+                        count += 1
+                    else:
+                        return -1
+                return 1
             else:
-                if (len(open_parenthesis) > len(closed_parenthesis)):
-                    result = 1
+
+                for i in collection:
+                    if i in matching_parenthesis.keys():
+                        open_parenthesis.append(i)
+                    else:
+                        closed_parenthesis.append(i)
+
+                if len(open_parenthesis) == len(closed_parenthesis):
+                    result = 0
+                    _stop = True
                 else:
-                    result = -1
-            _stop = True
+                    if (len(open_parenthesis) > len(closed_parenthesis)):
+                        result = 1
+                    else:
+                        result = -1
+                _stop = True
 
-    return  result
-
+    return result
 
