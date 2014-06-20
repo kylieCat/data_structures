4fc52503a5dbbf9b85517cc21abd490782c9b30f
Ian Auld <imauld@gmail.com>
2014-06-20 13:56:44 -0700

    PEP-8 fixes

--- graph/graph.py
+++ graph/graph.py
@@ -1,7 +1,5 @@
 # coding=utf-8
-
 class Node(object):
-
     def __init__(self, value, neighbours= None):
         self.value = value
         self.name = "Node {}"
@@ -10,12 +8,9 @@ class Node(object):
         else:
             self.neighbours = []
 
-
 class Graph(object):
-
     def __init__(self, sequence=None):
         self.name_counter = 0
-
         if sequence:
             for node in sequence:
                 self.add_node(node)
@@ -23,19 +18,15 @@ class Graph(object):
             self.graph = {}
 
     def add_node(self, node):
-
         node.name = node.name.format(self.name_counter)
         self.name_counter += 1
-
         self.graph[node.name] = [node.value, node.neighbours]
 
     def add_edges(self, node1, node2):
-
         if node1.name not in self.graph.keys():
             self.add_node(node1)
         if node2.name not in self.graph.keys():
             self.add_node(node2)
-
         node1.neighbours.append(node2.name)
         node2.neighbours.append(node1.name)
 
@@ -46,12 +37,10 @@ class Graph(object):
             node_neighbour = node.neighbours
             for neighbour in node_neighbour:
                 self.graph[neighbour][1].remove(node.name)
-
             node.neighbours = []
             del self.graph[node.name]
 
     def del_edge(self, node1, node2):
-
         if node1.name in node2.neighbours and node2.name in node1.neighbours:
             node1.neighbours.remove(node2.name)
             node2.neighbours.remove(node1.name)
@@ -59,20 +48,20 @@ class Graph(object):
             raise LookupError
 
     def had_node(self, node):
-
         return node.name in self.graph.keys()
 
     def neighbours(self, node):
-
         if node.name not in self.graph.keys():
             raise KeyError
         else:
             return [self.graph[name] for name in node.neighbours]
 
 
-    def adjacent(self, node1, node2):
-
+    def adjacent(self, node1, node2)
         if node1.name not in self.graph.keys() or node2.name not in self.graph.keys():
             raise KeyError
         else:
             return node2.name in node1.neighbours
+
+    def depth_first_traversal(self, start):
+        
--- graph/test_graph.py
+++ graph/test_graph.py
@@ -1,117 +1,92 @@
 # coding=utf-8
-
 import pytest
-
 from graph import Graph, Node
 
+
 def test_add_node():
-
     _graph = Graph()
-    _node = Node(4)
-
-    _graph.add_node(_node)
-
+    _graph.add_node(Node(4))
     assert _graph.graph == {'Node 0': [4, []]}
 
 
 def test_add_edge():
-
     _graph = Graph()
     _node1 = Node(4)
     _node2 = Node(5)
-
     _graph.add_edges(_node1, _node2)
-
-    assert _graph.graph == {'Node 0': [4, ['Node 1']], 'Node 1': [5, ['Node 0']]}
+    assert _graph.graph == {'Node 0': [4, ['Node 1']],
+                            'Node 1': [5, ['Node 0']]}
 
 
 def test_del_node():
-
     _graph = Graph()
     _node1 = Node(4)
     _node2 = Node(5)
-
     _graph.add_edges(_node1, _node2)
     _graph.del_node(_node2)
-
     assert _graph.graph == {'Node 0': [4, []]}
-
     _node3 = Node(6)
     _node4 = Node(7)
-
     _graph.add_edges(_node1, _node2)
     _graph.add_edges(_node3, _node4)
     _graph.add_edges(_node2, _node4)
-
     _graph.del_node(_node4)
-
     assert _graph.graph == {'Node 0' : [4, ['Node 1']],
                             'Node 1' : [5, ['Node 0']],
                             'Node 3' : [6, []]}
 
 
 def test_del_edge():
-
     _graph = Graph()
-
     _node1 = Node(4)
     _node2 = Node(5)
     _node3 = Node(6)
     _node4 = Node(7)
-
     _graph.add_edges(_node1, _node2)
     _graph.add_edges(_node3, _node4)
     _graph.add_edges(_node2, _node4)
-
     _graph.del_edge(_node2, _node4)
-
     assert _graph.graph == {'Node 0' : [4, ['Node 1']], 
                             'Node 1' : [5, ['Node 0']], 
                             'Node 2' : [6, ['Node 3']], 
                             'Node 3' : [7, ['Node 2']]}
 
 
+
 def test_has_node():
-
     _graph = Graph()
-
     _node1 = Node(4)
     _node2 = Node(5)
-
     _graph.add_node(_node1)
-
     assert _graph.had_node(_node1) == True
     assert _graph.had_node(_node2) == False
 
+
 def test_neighbours():
-
     _graph = Graph()
-
     _node1 = Node(4)
     _node2 = Node(5)
     _node3 = Node(6)
     _node4 = Node(7)
-
     _graph.add_edges(_node1, _node2)
     _graph.add_edges(_node3, _node4)
     _graph.add_edges(_node2, _node4)
-
     assert _graph.neighbours(_node2) == [[4, ['Node 1']], [7, ['Node 2', 'Node 1']]]
 
+
 def test_adjacent():
-
     _graph = Graph()
-
     _node1 = Node(4)
     _node2 = Node(5)
     _node3 = Node(6)
     _node4 = Node(7)
-
     _graph.add_edges(_node1, _node2)
     _graph.add_edges(_node3, _node4)
     _graph.add_edges(_node2, _node4)
-
     assert _graph.adjacent(_node1, _node2) == True
     assert _graph.adjacent(_node3, _node4) == True
     assert _graph.adjacent(_node2, _node4) == True
     assert _graph.adjacent(_node1, _node4) == False
+    
+def test_depth_traversal():
+    
