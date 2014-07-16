47933f0e7e1e71e4790d6916479737c2141661a7
Ian Auld <imauld@gmail.com>
2014-07-16 14:06:01 -0700

    implemented merge sort, untested

--- sorts/sorts.py
+++ sorts/sorts.py
@@ -7,25 +7,50 @@ def insertion(lst):
             idx -= 1
 
 
-# function merge_sort(list m)
-#     // Base case. A list of zero or one elements is sorted, by definition.
-#     if length(m) <= 1
-#         return m
-#
-#     // Recursive case. First, *divide* the list into equal-sized sublists.
-#     var list left, right
-#     var integer middle = length(m) / 2
-#     for each x in m before middle
-#          add x to left
-#     for each x in m after or equal middle
-#          add x to right
-#
-#     // Recursively sort both sublists.
-#     left = merge_sort(left)
-#     right = merge_sort(right)
-#     // *Conquer*: merge the now-sorted sublists.
-#     return merge(left, right)
 
 def merge(lst):
     if not lst or len(lst) < 2:
         return lst
+    left, right = lst[:len(lst) >> 1], lst[len(lst) >> 1:]
+    left = merge(left)
+    right = merge(right)
+    return _merge(left, right)
+
+
+# function merge(left, right)
+#     var list result
+#     // assign the element of the sublists to 'result' variable until there is no element to merge.
+#     while length(left) > 0 or length(right) > 0
+#         if length(left) > 0 and length(right) > 0
+#            // compare the first two element, which is the small one, of each two sublists.
+#             if first(left) <= first(right)
+#                 append first(left) to result
+#                 left = rest(left)
+#             else
+#                 append first(right) to result
+#                 right = rest(right)
+#         else if length(left) > 0
+#             append first(left) to result
+#             left = rest(left)
+#         else if length(right) > 0
+#             append first(right) to result
+#             right = rest(right)
+#     end while
+#     return result
+def _merge(left, right):
+    result = []
+    while len(left) and len(right):
+        if len(left) and len(right):
+            if left[0] <= right[0]:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+        elif len(left):
+            result.append(left[0])
+            left = left[1:]
+        elif len(right):
+            result.append(right[0])
+            right = right[1:]
+    return result
