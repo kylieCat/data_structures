a998a9fb43114409104d5d758f309c14a299fdf2
Ian Auld <imauld@gmail.com>
2014-07-28 18:12:30 -0700

    Added tests for proper parens back to repo

--- proper_parens/parens.py
+++ proper_parens/parens.py
@@ -2,29 +2,16 @@ def proper_parens(parens):
     """
     Takes a unicode string of parenthesis and checks to see if it is balanced.
     Returns:
-    1 - String is open, too many (
-    0 - String is balanced
-    -1 - String is broken, too many )
+    1: String is open, too many '('
+    0: String is balanced
+    -1: String is broken, too many ')'
     """
-    def check_braces(expressions):
-        for expression in expressions:
-            if _check_expression(expression):
-                print(1)
-            else:
-                print(0)
-
-    def _check_expression(expression):
-        pairs = {'{': '}', '(': ')', '[': ']'}
-        stack = []
-        for char in expression:
-            if char in ['{', '(', '[']:
-                stack.append(char)
-            else:
-                try:
-                    if pairs[stack.pop()] == char:
-                        continue
-                    else:
-                        raise IndexError
-                except IndexError:
-                    return False
-        return True
+    try:
+        valid_chars = {u'(': 1, u')': -1}
+        count = sum((
+            valid_chars[p] if p in valid_chars else str(p) for p in parens
+        ))
+        return int(bool(count)) if count >= 0 else -1
+    except TypeError:
+        print ('Invalid character in string')
+        raise ValueError()
