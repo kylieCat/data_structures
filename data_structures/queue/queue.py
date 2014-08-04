f5e7a02e51b3e54bfaba77ce23c531a0d7596f12
Ian Auld <imauld@gmail.com>
2014-08-04 17:06:12 -0400

    Cleaned up files for queue

--- data_structures/queue/queue.py
+++ data_structures/queue/queue.py
@@ -2,13 +2,13 @@ class Node(object):
     def __init__(self, value=None):
         self.value = value
         self.next = None
-    
+
     def __str__(self):
         return str(self.value)
-        
+
     def __repr__(self):
         return str(self.value)
-  
+
 
 class Queue(object):
     def __init__(self):
@@ -24,21 +24,15 @@ class Queue(object):
             node = Node(value)
             self.tail.next, self.tail = node, node
         self._size += 1
-    
+
     def dequeue(self):
         if not self.size():
             raise LookupError()
         else:
-            return_value = self.head.value
             old_head, self.head = self.head, self.head.next
             old_head.next = None
             self._size -= 1
-            return return_value
-    
+            return old_head.value
+
     def size(self):
         return self._size
-        
-        
-        
-        
-        
--- data_structures/queue/test_queue.py
+++ data_structures/queue/test_queue.py
@@ -1,16 +1,17 @@
 import pytest
-from queue import Queue
+from data_structures.queue import Queue
 
 
 def test_queue():
     q = Queue()
     assert q is not None
-    
+
+
 def test_enqueue():
     q = Queue()
     q.enqueue(1)
     assert q.head.value == 1
-    assert q.head.next == None
+    assert q.head.next is None
     assert q.tail.value == 1
     q.enqueue(2)
     assert q.head.value == 1
@@ -21,24 +22,28 @@ def test_enqueue():
     assert q.head.next.value == 2
     assert q.tail.value == 3
 
+
+def test_dequeue_empty():
+    with pytest.raises(LookupError):
+        Queue().dequeue()
+
+
 def test_dequeue():
     q = Queue()
-    for i in range(1,4):
+    for i in range(1, 4):
         q.enqueue(i)
     assert q.dequeue() == 1
     assert q.dequeue() == 2
     assert q.dequeue() == 3
-    with pytest.raises(LookupError):
-        q.dequeue()
-    
+
+
 def test_size():
     q = Queue()
     assert q.size() == 0
-    for i in range(1,4):
+    for i in range(1, 4):
         q.enqueue(i)
     assert q.size() == 3
     q.dequeue()
     assert q.size() == 2
     q.dequeue()
     assert q.size() == 1
-    
