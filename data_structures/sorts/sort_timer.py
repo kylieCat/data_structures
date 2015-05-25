958a82dd46fcd8e231e097a43aeb6bfe8e80a267
Ian Auld <imauld@gmail.com>
2015-05-25 14:36:16 -0700

    Chnaged some values to make test more practical

--- data_structures/sorts/sort_timer.py
+++ data_structures/sorts/sort_timer.py
@@ -15,18 +15,18 @@ hundred_thousand = 'lst = [randint(1,1000) for _ in range(100000)]'
 million = 'lst = [randint(1,1000) for _ in range(1000000)]'
 
 # Insertion Sort timing
-print('Staring insertion sort 10,000 elements')
+print('Starting insertion sort 10,000 elements')
 m = min(
     timeit.Timer(
         'l = lst[:]; insertion(lst)',
-        setup=setup+ten_thousand).repeat(1, 1000)
+        setup=setup+ten_thousand).repeat(1, 2)
 )
 print('Minimum time: {}'.format(m))
-print('Staring insertion sort 100,000 elements')
+print('Starting insertion sort 100,000 elements')
 m = min(
     timeit.Timer(
         'l = lst[:]; insertion(lst)',
-        setup=setup+hundred_thousand).repeat(1, 1000)
+        setup=setup+hundred_thousand).repeat(1, 2)
 )
 print('Minimum time: {}'.format(m))
 
@@ -35,29 +35,29 @@ print('Staring merge sort 10,000 elements')
 m = min(
     timeit.Timer(
         'l = lst[:]; merge(lst)',
-        setup=setup+ten_thousand).repeat(1, 1000)
+        setup=setup+ten_thousand).repeat(1, 2)
 )
 print('Minimum time: {}'.format(m))
-print('Staring merge sort 100,000 elements')
+print('Starting merge sort 100,000 elements')
 m = min(
     timeit.Timer(
         'l = lst[:]; merge(lst)',
-        setup=setup+hundred_thousand).repeat(1, 1000)
+        setup=setup+hundred_thousand).repeat(1, 2)
 )
 print('Minimum time: {}'.format(m))
 
 # Quick Sort timing
-print('Staring quick sort 10,000 elements')
+print('Starting quick sort 10,000 elements')
 m = min(
     timeit.Timer(
         'l = lst[:]; quick(lst)',
-        setup=setup+ten_thousand).repeat(1, 1000)
+        setup=setup+ten_thousand).repeat(1, 2)
 )
 print('Minimum time: {}'.format(m))
-print('Staring quick sort 100,000 elements')
+print('Starting quick sort 100,000 elements')
 m = min(
     timeit.Timer(
         'l = lst[:]; quick(lst)',
-        setup=setup+hundred_thousand).repeat(1, 1000)
+        setup=setup+hundred_thousand).repeat(1, 2)
 )
 print('Minimum time: {}'.format(m))
